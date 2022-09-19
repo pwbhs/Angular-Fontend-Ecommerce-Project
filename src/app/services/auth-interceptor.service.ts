02161451a3de16ef6a71388182393adc41744d4e
Wenbo Peng <wn439756@dal.ca>
2022-09-18 21:59:23 -0300

    cert

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,47 +1,43 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
-import { from, lastValueFrom, Observable } from 'rxjs';
-
+import { from, Observable } from 'rxjs';
+ 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService implements HttpInterceptor{
-
-  // inject OktaAuthService
-  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
-  
-  // Will intercept all outgoing HTTP requests of HttpClient
+export class AuthInterceptorService {
+ 
+  constructor(private oktaAuthService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+ 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
     return from(this.handleAccess(request, next));
   }
-
-
-  private async handleAccess(request: any, next: HttpHandler): Promise<HttpEvent<any>> {
-    
-    // Only add an access token for secured endpoint
+ 
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> { 
+ 
+    // Only add an access token for secured endpoints
+    // const theEndpoint = environment.luv2shopApiUrl + '/orders';
+    // const securedEndpoints = [theEndpoint];
+ 
     const securedEndpoints = ['http://localhost:8080/api/orders'];
-
-    if (securedEndpoints.some(url => request.urlWithPatams.includes(url))) {
-
+    
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+ 
       // get access token
       const accessToken = await this.oktaAuth.getAccessToken();
-    
+ 
       // clone the request and add new header with access token
       request = request.clone({
-        setHeader: {
+        setHeaders: {
           Authorization: 'Bearer ' + accessToken
         }
-      })
-
-      return await lastValueFrom(next.handle(request)); //continue other interceptors in the chain.
-      //If there are no interceptors , make the call to REST API
+      });
+ 
     }
-
+ 
+    return next.handle(request).toPromise();
   }
-
 }
-
-
